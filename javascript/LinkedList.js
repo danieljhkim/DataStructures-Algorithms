8077161986954d0b55161b6812fa22590657350a
danieljhkim <danieljhkim1@gmail.com>
2023-06-24 14:00:35 -0400

    linked list solution

--- javascript/LinkedList.js
+++ javascript/LinkedList.js
@@ -40,3 +40,46 @@ var removeElements = function(head, val) {
   }
   return head.val===val?head.next:head;
 };
+
+var findDuplicate = function(nums) {
+  nums.sort();
+  for(let i=0; i<nums.length-1; i++) {
+    if(nums[i]===nums[i+1]) return nums[i];
+  }
+};
+
+var wordPattern = function(pattern, s) {
+  const ar = s.split(" ");
+  if(pattern.length !== ar.length) return false;
+  const keyMap = {};
+  const valueMap = {};
+  for(let i=0; i<ar.length; i++) {
+    const key = pattern[i];
+    const val = ar[i];
+    if(key in keyMap || val in valueMap) {
+      if(keyMap[key] !== val) return false;
+      if(valueMap[val] !== key) return false;
+    } else {
+      keyMap[key] = val;
+      valueMap[val] = key;
+    }
+  }
+  return true;
+};
+
+var dayOfTheWeek = function(day, month, year) {
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  const dateObj = new Date(year, month, day);
+  return weekday[dateObj.getDay()];
+};
+
+var isPrefixOfWord = function(sentence, searchWord) {
+  let ar = sentence.split(" ");
+  let len = searchWord.length;
+  for(let i=0; i<ar.length; i++) {
+    let word = ar[i].splice(0, len);
+    if(word === searchWord) return i;
+  }
+  return -1;
+};
