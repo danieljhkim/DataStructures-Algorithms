664fae87e6c8129126200ab1d1d545658a02f50c
danieljhkim <danieljhkim1@gmail.com>
2024-01-20 16:41:11 -0500

    more hash power

--- java/Leetcode/src/com/leetcode/HashMapSolutions.java
+++ java/Leetcode/src/com/leetcode/HashMapSolutions.java
@@ -5,7 +5,12 @@ import java.util.HashMap;
 public class HashMapSolutions {
 	
     public static void main(String[] args) {
-    	boolean ans = canConstruct("aa", "aba");
+
+        int[] map1 = new int[256];
+        String st = "abc";
+        map1[st.charAt(0)] = 5;
+        System.out.println(map1[st.charAt(0)]);
+        boolean ans = isIsomorphic("abc", "dga");
         System.out.println(ans);
     }
     
@@ -32,4 +37,35 @@ public class HashMapSolutions {
         }
         return true;
     }
+    
+    //https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&envId=top-interview-150
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+        	return false;
+        }
+        
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+        
+        for (int i=0; i<s.length(); i++) {
+        	if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
+        		return false;
+        	}
+        	map1[s.charAt(i)] = i + 1;
+        	map2[t.charAt(i)] = i + 1;
+        }
+        return true;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
