c9cd97f83ee08b0d58aa83a371b3431ced8f7aba
danieljhkim <danieljhkim1@gmail.com>
2024-01-23 22:08:15 -0500

    java easy

--- java/Leetcode/src/com/leetcode/solutions.java
+++ java/Leetcode/src/com/leetcode/solutions.java
@@ -70,5 +70,24 @@ class Solution {
         return ans;
     }
     
+    //https://leetcode.com/problems/find-the-maximum-achievable-number/
+    public int theMaximumAchievableX(int num, int t) {
+        return t*2 + num;
+    }
+    
+    //https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+    public int differenceOfSums(int n, int m) {
+    	int num1 = 0;
+    	int num2 = 0;
+        for (int i=1; i<=n; i++) {
+        	if (i%m != 0) {
+        		num1 += i;
+        	} else {
+        		num2 += i;
+        	}
+        }
+        return num1-num2;
+    }
+    
     
 }
