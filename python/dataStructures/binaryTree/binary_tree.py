01efa1e5e67d64d7404cdf390aa1e45f395dab78
danieljhkim <danieljhkim1@gmail.com>
2024-02-03 19:16:17 -0500

    tree in python

---
+++ python/dataStructures/binaryTree/binary_tree.py
@@ -0,0 +1,43 @@
+
+
+class Node:
+  def __init__(self, key):
+    self.left = None
+    self.right = None
+    self.val = key
+
+'''
+        1
+      2   3
+    4  5  6 7
+
+'''
+
+def inorderTraversal(root):
+  # tranverses nodes in non-decreasing order
+  # 4 2 5 1 6 3 7  
+  if root:
+    inorderTraversal(root.left)
+    print(root.val)
+    inorderTraversal(root.right)
+
+def preorderTraversal(root):
+  # used to create a copy of the tree
+  # used to get prefix expressions of an expression tree
+  # 1 2 4 5 3 6 7 
+  if root:
+    print(root.val)
+    preorderTraversal(root.left)
+    preorderTraversal(root.right)
+
+def postorderTraversal(root):
+  # used to delete a tree
+  # useful for getting postrix expression 
+  # 4 5 2 6 7 3 1
+  if root:
+    postorderTraversal(root.left)
+    postorderTraversal(root.right)
+    print(root.val)
+
+
+
