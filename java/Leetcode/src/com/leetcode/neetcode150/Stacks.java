ff30a6373904c3ad0fb1709ddadafadd69a0a8bd
danieljhkim <danieljhkim1@gmail.com>
2024-09-07 20:07:33 -0400

    stacks

---
+++ java/Leetcode/src/com/leetcode/neetcode150/Stacks.java
@@ -0,0 +1,31 @@
+package Leetcode.src.com.leetcode.neetcode150;
+import java.util.Stack;
+import java.util.*;
+
+public class Stacks {
+
+    // 22. Generate Parentheses
+    public List<String> generateParenthesis(int n) {
+        Stack<String[]> stack = new Stack<>();
+        List<String> ans = new ArrayList<>();
+        String [] stuff = {"(", Integer.toString(n - 1), Integer.toString(n)};
+        stack.add(stuff);
+        while (!stack.isEmpty()) {
+            String[] stuff1 = stack.pop();
+            int open = Integer.parseInt(stuff1[1]);
+            int close = Integer.parseInt(stuff1[2]);
+            String ss = stuff1[0];
+            if (open == 0 && close == 0) {
+                ans.add(ss);
+                continue;
+            }
+            if (open > 0) {
+                stack.add(new String[] {ss + "(", Integer.toString(open - 1), Integer.toString(close)});
+            }
+            if (open < close) {
+                stack.add(new String[] {ss + ")", Integer.toString(open), Integer.toString(close - 1)});
+            }
+        }
+        return ans;
+    }
+}
