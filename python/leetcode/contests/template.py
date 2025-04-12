d1045fdcc9576e10392a4dccb0176c6953b7b9b2
Daniel <33708658+danieljhkim@users.noreply.github.com>
2025-04-12 16:02:30 -0400

    style: organize contest dir

---
+++ python/leetcode/contests/template.py
@@ -0,0 +1,43 @@
+import heapq
+import random
+import math
+import bisect
+from typing import *
+from math import inf, factorial, gcd, lcm
+from functools import lru_cache, cache
+from heapq import heapify, heappush, heappop
+from itertools import accumulate, permutations, combinations
+from collections import Counter, deque, defaultdict, OrderedDict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    pass
+
+
+def test_solution():
+    s = Solution()
+
+
+if __name__ == "__main__":
+    test_solution()
